886da3f88d9cdf0b0bd6242ce7b89a3de34dc078
Todor Kostov <todpavkos@gmail.com>
2022-09-16 21:31:31 +0200

    some changes

--- JS Fundamentals/01-bs-conditional-statements-loops-Exercise/02-Rounding.js
+++ JS Fundamentals/01-bs-conditional-statements-loops-Exercise/02-Rounding.js
@@ -1,12 +1,8 @@
 function solve(n1,n2){
 
-    if(n2>15){
-    n2=15
-    }
-    
-
+    if(n2>15){n2=15}
     console.log(parseFloat(n1.toFixed(n2)))
 
 }
 
-solve(10.5,18)
+solve(10.5454754745,18)
